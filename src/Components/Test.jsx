9f0b27e413c9bbaa7ff080b9be44d9980f5c6a9c
Babul Akter <babulakterfsd@gmail.com>
2022-06-03 10:33:34 +0600

    counter added in the test component

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Test() {
+    const [counter, setCounter] = useState(0);
+    const handleCounter = () => {
+        setCounter(counter + 1);
+    };
     return (
-        <div>
-            <p>This is the test component</p>
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                minHeight: '100vh',
+            }}
+        >
+            <h1>{counter}</h1>
+            <button type="button" onClick={() => handleCounter()}>
+                Increment
+            </button>
         </div>
     );
 }

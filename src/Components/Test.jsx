91eb072d79c1620fecdf7f947e2566a4383d7877
Babul Akter <babulakterfsd@gmail.com>
2022-06-05 18:31:08 +0600

    test component added

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -1,24 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function Test() {
-    const [counter, setCounter] = useState(0);
-    const handleCounter = () => {
-        setCounter(counter + 1);
-    };
     return (
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                minHeight: '100vh',
-            }}
-        >
-            <h1>{counter}</h1>
-            <button type="button" onClick={() => handleCounter()}>
-                Increment
-            </button>
+        <div>
+            <h1>Hello world</h1>
         </div>
     );
 }

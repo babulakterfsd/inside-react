dd54cb094cf7787b48dabfc8c49a3b7e678b0217
Babul Akter <babulakterfsd@gmail.com>
2022-06-20 13:30:52 +0600

    redicer added

--- src/Pages/Home2.jsx
+++ src/Pages/Home2.jsx
@@ -2,13 +2,18 @@ import { useReducer } from 'react';
 
 const initialState = {
     counter: 0,
+    counter1: 0,
 };
 const myReducer = (state, action) => {
     switch (action.type) {
         case 'increment':
-            return { counter: state.counter + action.value };
+            return { ...state, counter: state.counter + action.value };
         case 'decrement':
-            return { counter: state.counter - action.value };
+            return { ...state, counter: state.counter - action.value };
+        case 'increment1':
+            return { ...state, counter1: state.counter1 + action.value };
+        case 'decrement1':
+            return { ...state, counter1: state.counter1 - action.value };
         default:
             return { counter: state.counter };
     }
@@ -27,7 +32,10 @@ function Home2() {
                 flexDirection: 'column',
             }}
         >
-            <h1 style={{ marginBottom: '30px' }}>The count is {count.counter} now</h1>
+            <h1 style={{ marginBottom: '30px' }}>The counter is {count.counter} now</h1>
+            <h1 style={{ marginBottom: '30px', color: 'green' }}>
+                The counter1 is {count.counter1} now
+            </h1>
             <div
                 style={{
                     display: 'flex',
@@ -46,9 +54,9 @@ function Home2() {
                 <button
                     type="button"
                     style={{ marginRight: '30px' }}
-                    onClick={() => dispatch({ type: 'increment', value: 5 })}
+                    onClick={() => dispatch({ type: 'decrement', value: 1 })}
                 >
-                    increment by 5
+                    decrement by 1
                 </button>
             </div>
 
@@ -61,13 +69,17 @@ function Home2() {
             >
                 <button
                     type="button"
-                    onClick={() => dispatch({ type: 'decrement', value: 1 })}
-                    style={{ marginRight: '30px', marginLeft: '-30px' }}
+                    style={{ marginRight: '30px' }}
+                    onClick={() => dispatch({ type: 'increment1', value: 1 })}
                 >
-                    decrement by 1
+                    increment1 by 1
                 </button>
-                <button type="button" onClick={() => dispatch({ type: 'decrement', value: 5 })}>
-                    decrement by 5
+                <button
+                    type="button"
+                    style={{ marginRight: '30px' }}
+                    onClick={() => dispatch({ type: 'decrement1', value: 1 })}
+                >
+                    decrement1 by 1
                 </button>
             </div>
         </div>

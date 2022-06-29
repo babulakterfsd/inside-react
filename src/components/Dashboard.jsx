9477786d5ff37bce624541e60dd79d2875a5dd23
Babul Akter <babulakterfsd@gmail.com>
2022-06-29 11:52:41 +0600

    dashboard generated depending on user role

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 function Dashboard() {
+    const { user, admin } = useAuth();
+
     return (
         <div>
-            <p>This is the dashboard</p>
+            <p>
+                This is an {admin ? `Admin` : `User`} Dashboard for {user}
+            </p>
             <Link to="/">
                 <button type="button">Back to home</button>
             </Link>
